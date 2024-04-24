68077f4df4d097011693fc822cb74f79d9b36980
Vishal V Poojary <vishalvpoojary1@gmail.com>
2024-04-24 21:57:07 +0530

    Add home button in header

--- client/src/components/AppContainer/index.js
+++ client/src/components/AppContainer/index.js
@@ -4,11 +4,17 @@ import { createApolloClient } from "@/graphql/client";
 import { ApolloProvider } from "@apollo/client";
 import Cart from "../Cart";
 import ProfileWidget from "../ProfileWidget";
+import { HomeOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { PATHS } from "@/app-constants";
 
 const AppContainer = ({ children }) => {
   const client = createApolloClient();
   return (
     <ApolloProvider client={client}>
+      <Link href={PATHS.PRODUCTS}>
+        <HomeOutlined />
+      </Link>
       <Cart />
       <ProfileWidget />
       {children}
